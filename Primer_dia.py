5ed8e3945e57acea0ff062ed97569002d5cd5b0c
Taratara <martitarrason@gmail.com>
2024-09-17 12:59:53 +0200

    Operaciones
    Uso de operaciones

--- Primer_dia.py
+++ Primer_dia.py
@@ -1,1 +1,12 @@
 print("hello word")
+#Para compilar el programa, usar la flecha de arriba a la derecha (visual code)
+
+#Ejercicios de operaciones de clase
+print (9 // 5) 
+print (695 % 20) 
+print (7 + 6 * 5) 
+print (7 * 6 + 5) 
+print (248 % 100 / 5) 
+print (6 * 3 - 9 // 4) 
+print ((5 - 7) * 2 ** 2) 
+print (6 + (18 % (17 - 12))) 
